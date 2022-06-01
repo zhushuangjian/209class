ef2934697c30c713e31637b4ab06318d948f77df
zhushuangjian <59001341+zhushuangjian@users.noreply.github.com>
2022-06-01 17:42:27 +0800

    Update hello.c
    add  3 printf

--- hello.c
+++ hello.c
@@ -5,5 +5,8 @@ int main(int argc, const char *argv[])
 	printf("hello world!\n");
 	printf("hello world!\n");
 	printf("hello world!\n");
+	printf("hello world!\n");
+	printf("hello world!\n");
+	printf("hello world!\n");
 	return 0;
 }
